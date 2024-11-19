a81d763fade431c3de8775cf4c792927b42cfa0b
xocass <joaquin.quintas@rai.usc.es>
2024-11-19 20:12:58 +0100

    prueba

--- src/main/java/p5/Client/Client.java
+++ src/main/java/p5/Client/Client.java
@@ -1,14 +1,20 @@
 package p5.Client;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import p5.Client.controllers.cRegistrarse;
 import p5.Server.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.rmi.Naming;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Client extends Application{
+
     ServerInterface server;
     public static void main(String args[]) {
         launch();
@@ -16,8 +22,14 @@ public class Client extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("VRegistrarse.fxml"));
+        primaryStage.setTitle("prueba");
+        primaryStage.setScene(new Scene(loader.load(), 385, 216));
+        primaryStage.show();
+        cRegistrarse controller = loader.getController();
         registroRMI();
-        server.registrarUsuario("mati","mati");
+        controller.setServer(server);
+        //server.registrarUsuario("mati","mati");
     }
 
 
--- src/main/java/p5/Client/controllers/cRegistrarse.java
+++ src/main/java/p5/Client/controllers/cRegistrarse.java
@@ -1,4 +1,28 @@
 package p5.Client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import p5.Server.ServerInterface;
+
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+
 public class cRegistrarse {
+    private ServerInterface server;
+    @FXML
+    private TextField nick;
+    @FXML
+    private PasswordField passwd;
+    @FXML
+    private Button registrar;
+
+    public void setServer(ServerInterface server){
+        this.server=server;
+    }
+    @FXML
+    public void clickRegistrar() throws SQLException, RemoteException {
+        server.registrarUsuario(nick.getText(),passwd.getText());
+    }
 }
