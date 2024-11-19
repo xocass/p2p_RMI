fc3bf7dddebefe5db470a1aef9e2fb1f5d565540
xocass <joaquin.quintas@rai.usc.es>
2024-11-19 18:31:33 +0100

    prueba

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module p5.comdis_p5 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens p5.comdis_p5 to javafx.fxml;
---
+++ src/main/java/p5/comdis_p5/BBDD.java
@@ -0,0 +1,12 @@
+package p5.comdis_p5;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class BBDD {
+
+}
