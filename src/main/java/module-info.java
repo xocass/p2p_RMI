663c01c892e94bf21daff254bea53f2a5673dd48
xocass <joaquin.quintas@rai.usc.es>
2024-11-19 19:39:50 +0100

    prueba

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,6 @@ module p5.comdis_p5 {
     exports p5.Server;
     exports p5.Client;
     opens p5.Client to javafx.fxml;
+    exports p5.Client.controllers;
+    opens p5.Client.controllers to javafx.fxml;
 }
