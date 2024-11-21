454e884c025a3042cc4b1fc3511db853072520a6
xocass <joaquin.quintas@rai.usc.es>
2024-11-21 18:35:47 +0100

    prueba

---
+++ src/main/java/p5/Client/controllers/CPrincipal.java
@@ -0,0 +1,21 @@
+package p5.Client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class CPrincipal {
+    @FXML
+    private VBox amigos;
+    @FXML
+    private Button solicitudes;
+    @FXML
+    private VBox chat;
+    @FXML
+    private TextField msg;
+    @FXML
+    private Button enviar;
+
+
+}
