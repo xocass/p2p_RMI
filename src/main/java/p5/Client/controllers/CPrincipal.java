a04790939c4dc5ee16011f43d15396324ce67727
xocass <joaquin.quintas@rai.usc.es>
2024-11-25 17:50:51 +0100

    prueba

--- src/main/java/p5/Client/controllers/CPrincipal.java
+++ src/main/java/p5/Client/controllers/CPrincipal.java
@@ -1,14 +1,18 @@
 package p5.Client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import p5.Server.ServerInterface;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class CPrincipal {
     @FXML
-    private VBox amigos;
+    private VBox boxAmigos;
     @FXML
     private Button solicitudes;
     @FXML
@@ -20,7 +24,13 @@ public class CPrincipal {
     private ServerInterface server;
 
     @FXML
-    public void init(){
-
+    public void init(ArrayList<String> amigos) throws IOException {
+        for(String s:amigos){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("p5.client.VTemplateAmigo.fxml"));
+            boxAmigos.getChildren().add(loader.load());
+            CTemplateAmigo controller = loader.getController();
+            controller.setNick(s);
+        }
     }
+
 }
